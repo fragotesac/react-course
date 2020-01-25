24d2dbd4873172b8df6e84c261e93fd63030aa7d
Francis Gonzales <fgonzalestello91@gmail.com>
2020-01-25 12:08:25 -0500

    manejos de strings en JSX

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const App = <h1>Hello World -  React</h1>
+const name = 'Francis Gonzales'
+const user = {
+    'id': 1,
+    'name': 'Francis',
+    'edad': 28
+}
+function userId(user) {
+    return user.id
+}
+
+const App = <h1>
+    Hello World -  React By { name } mi edad es { user.edad }
+    mi id es { userId(user) }
+</h1>
 
 const root = document.getElementById('root')
 ReactDom.render(App, root)
